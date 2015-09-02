9afeb276f0c765bee87c7c627cea4b832708e66d
Brenton Morris <brenton.morris@odinsol.com>
2015-09-03 10:40:41 +1200

    Fixed mixed tabs and spaces in documentation.

--- doc.go
+++ doc.go
@@ -8,20 +8,20 @@ First we create a WorkerQueue that can hold up to 5 workers
 Next we create 5 workers and start each one
 
 	for i := range workers {
-        workers[i] = gotowork.NewWorker(i, workerQueue)
-        workers[i].Start()
-    }
+		workers[i] = gotowork.NewWorker(i, workerQueue)
+		workers[i].Start()
+	}
 
 Then we add some jobs to the queue
 
 	for i := 0; i < 5; i++ {
-        // get a free worker
-        worker := <-workerQueue
-        // give it some work (this can be any function)
-        worker <- func () {
-            fmt.Println("Hello")
-        }
-    }
+		// get a free worker
+		worker := <-workerQueue
+		// give it some work (this can be any function)
+		worker <- func () {
+			fmt.Println("Hello")
+		}
+	}
 
 Once all work has been dispatched we tell the workers to stop
 
@@ -34,6 +34,5 @@ Then wait for all the workers to finish the current jobs
     for i := range workers {
         workers[i].WaitForFinish()
     }
-
 */
 package gotowork
